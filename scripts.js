e6275b3cac2fb5c54c5b7db6d2648c07d3096520
realPaulSwan <paul.swan@wsu.edu>
2023-04-29 15:17:20 -0700

    Added Wallpaper and fixed bugs

--- scripts.js
+++ scripts.js
@@ -1,28 +1,56 @@
+var latitude = 0;
+var longitude = 0;
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8cbd3a1197mshd38b28f38b92abdp1182c2jsn9dc19b670992',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
-
-// Retrieve current weather data from WeatherAPI.com
-fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13', options)
+fetch('https://ipapi.co/json/')
   .then(response => response.json())
-  .then(data => {
-    // Retrieve weather information from API response
-    const temperature = data.current.temp_c;
-    const wind_direction = data.current.wind_dir;
-    const wind_speed = data.current.wind_kph;
-    const cloud_cover = data.current.cloud;
-    const humidity = data.current.humidity;
+  .then(data2 => {
+    // Log the location data to the console
+    const location = data2.city;
+    const state = data2.region;
+    latitude = data2.latitude;
+    longatude = data2.longitude;
+    console.log(data2);
+    document.getElementById("location").innerHTML = `${location}, ${state}`;
 
-    // Update the HTML elements with the weather information
-    document.getElementById("temperature").innerHTML = `${temperature} &deg;C`;
-    document.getElementById("wind-direction").innerHTML = wind_direction;
-    document.getElementById("wind-speed").innerHTML = `${wind_speed} km/h`;
-    document.getElementById("cloud-cover").innerHTML = `Cloud Cover: ${cloud_cover}%`;
-    document.getElementById("humidity").innerHTML = `Humidity: ${humidity}%`;
+
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '8cbd3a1197mshd38b28f38b92abdp1182c2jsn9dc19b670992',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    };
+    
+    // Retrieve current weather data from WeatherAPI.com
+    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=%20'+ data2.latitude + '%2C' + data2.longitude, options)
+      .then(response => response.json())
+      .then(data => {
+        // Retrieve weather information from API response
+        //const location = data.current.location;
+        //const latitude = data.current.latitude;
+        console.log(data);
+        const temperature = data.current.temp_f;
+        const wind_direction = data.current.wind_dir;
+        const wind_speed = data.current.wind_kph;
+        const cloud_cover = data.current.cloud;
+        const humidity = data.current.humidity;
+    
+        // Update the HTML elements with the weather information
+        //document.getElementById("location").innerHTML = `${location}`;
+        document.getElementById("temperature").innerHTML = `${temperature} &deg;F`;
+        //document.getElementById("wind-direction").innerHTML = ` `,;
+        document.getElementById("wind-speed").innerHTML = `Wind: ${wind_direction}, ${wind_speed} km/h`;
+        document.getElementById("cloud-cover").innerHTML = `Cloud Cover: ${cloud_cover}%`;
+        document.getElementById("humidity").innerHTML = `Humidity: ${humidity}%`;
+      })
+      .catch(error => console.log(error));
+
+
+
   })
-  .catch(error => console.log(error));
+  .catch(error => {
+    // Log any errors to the console
+    console.error(error);
+  });
+
